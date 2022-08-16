9fd0e405161f44d8f299d9622f564b8c1d1da40b
Huy <huy@bluebottle.digital>
2022-08-16 12:41:39 +1000

    huy

--- index.js
+++ index.js
@@ -1,10 +1,93 @@
 const express = require("express");
 const app = express();
 const AWS = require("aws-sdk");
-app.get("/",(req,res)=>{
-    res.send("hi")
+
+app.use(express.json());
+const awsConfig = {
+    "region": "us-west-2",
+    "endpoint": "http://dynamodb.us-west-2.amazonaws.com",
+    "accessKeyId": "AKIAZMS4P7E2QTQUFPSN", "secretAccessKey": "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"
+};
+
+AWS.config.update(awsConfig);
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+app.get("/users/:id", async(req, res) => {
+    var params = {
+        TableName: "users",
+        Key: {
+            "id": req.params.id
+        },
+        
+    };
+    
+    try {
+    const data = await docClient.get(params).promise();
+    if(data.Item!=null) {
+        console.log(data.Item.id);
+        console.log(data.Item.first_name);
+        console.log(data.Item.last_name);
+        res.send(data.Item);
+    }
+    else{
+        res.status(404).send("No Item matched");
+
+    }
+    }
+    catch(err){
+        if(err.message=="")
+        res.status(500).send(err.message);
+    }
 })
-app.post("/",(req,res)=>{})
-app.put("/",(req,res)=>{})
-app.delete("/",(req,res)=>{})
-app.listen(5000)
+app.post('/signIn', async (req, res) => {
+
+    
+    var params = {
+        TableName: "users",
+        Item:  req.body
+    };
+    try{
+
+        const data=await docClient.put(params).promise()
+        res.send("Welcome")
+    }
+    catch(err){res.status(500).send(err.message)}
+
+  })
+
+app.delete("/users/:id", async (req, res) => {
+
+    const params = {
+        TableName: "users",
+        Key: {
+            "id": req.params.id
+        }
+    };
+    try {
+        const data = await docClient.get(params).promise();
+        if(data.Item!=null) {
+            console.log(data.Item.id);
+            console.log(data.Item.first_name);
+            console.log(data.Item.last_name);
+            res.send(data.Item);
+        }
+        else{
+            
+            res.status(404).send("No Item matched");
+    
+        }
+        }
+        catch(err){
+            
+            res.status(500).send(err.message);
+        }
+    const data2= docClient.delete(params, function (err, data) {
+
+        if (err) {
+            console.log("users::delete::error - " + JSON.stringify(err, null, 2));
+        }
+    });
+})
+
+app.listen(2000)
